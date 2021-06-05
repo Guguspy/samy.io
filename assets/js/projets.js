c75120e4cc0e5a22784e8d0ddb432f010e0de034
Guguspy <reverdyguillaume73@gmail.com>
2021-06-05 03:10:51 +0200

    Add files via upload

---
+++ assets/js/projets.js
@@ -0,0 +1,15 @@
+document.getElementById("card_1_id").onclick = function() {
+    window.open("assets/pdf/Proj_Centreon.pdf");
+}
+
+document.getElementById("card_2_id").onclick = function() {
+    window.open("assets/pdf/Proj_WDS.pdf");
+}
+
+document.getElementById("card_3_id").onclick = function() {
+    window.open("assets/pdf/Proj_Wim.pdf");
+}
+
+document.getElementById("card_4_id").onclick = function() {
+    window.open("assets/pdf/Proj_Vpn.pdf");
+}
