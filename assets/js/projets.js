f156b3675b1eae3a76b65251a0e40a646c1fafe8
Guguspy <reverdyguillaume73@gmail.com>
2021-06-06 19:57:26 +0200

    Add files via upload

--- assets/js/projets.js
+++ assets/js/projets.js
@@ -7,7 +7,7 @@ document.getElementById("card_2_id").onclick = function() {
 }
 
 document.getElementById("card_3_id").onclick = function() {
-    window.open("assets/pdf/Proj_Wim.pdf");
+    window.open("assets/pdf/Proj_Veeam.pdf");
 }
 
 document.getElementById("card_4_id").onclick = function() {
